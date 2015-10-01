95ccecbb5ee765aa334d6804ef8a8cfcd6295087
jokie666 <joris_coenen-@hotmail.com>
2015-10-01 15:45:29 +0200

    icon

--- app/src/main/java/com/jjc/isaacwiki/MainActivity.java
+++ app/src/main/java/com/jjc/isaacwiki/MainActivity.java
@@ -1,5 +1,6 @@
 package com.jjc.isaacwiki;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,8 +9,13 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Intent itemIntent;
+
+    Button itemButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +23,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        //Voor joris om de singelItem Activity te maken
+        itemIntent = new Intent(this, singleItemActivity.class);
+        itemButton = (Button) findViewById(R.id.switchIntent);
+        itemButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(itemIntent);
+            }
+        });
     }
 
     @Override
